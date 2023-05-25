8202827df15589719d85057d376c15de915e21a8
Abdallahhn <abdallah.a99h@gmail.com>
2023-05-25 12:06:06 +0300

    ds

---
+++ p32-Fanctions/p32-Fanctions/p32-Fanctions.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void DisplayMyCardInfo()
+{
+    cout << "********************************\n";
+    cout << "Name: " << "Abdallah Hussein.\n";
+    cout << "Age: " << "24." << std::endl;
+    cout << "City: " << "Damascus.\n";
+    cout << "Country: " << "Syria.\n";
+    cout << "********************************\n\n";
+
+}
+
+void PrintSquareStars()
+{
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n\n";
+
+}
+
+void PrintIloveProgramming()
+{
+    cout << "I Love Programming!\n\n";
+    cout << "I promiseto be best developer ever!\n\n";
+    cout << "I know it will take some time to practice, but I will achieve my gool.\n\n";
+    cout << "Best Regards.\n";
+    cout << "Abdallah Hussein.\n\n";
+
+}
+
+void Print_H()
+{
+    cout << "*     *\n";
+    cout << "*     *\n";
+    cout << "*******\n";
+    cout << "*     *\n";
+    cout << "*     *\n";
+}
+
+int main()
+{
+    DisplayMyCardInfo();
+    PrintSquareStars();
+    PrintIloveProgramming();
+    Print_H();
+
+    return 0;
+}
