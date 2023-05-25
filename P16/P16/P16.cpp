25e9e6f08609965fcf77b586f71cc66279e4b7b0
Abdallahhn <abdallah.a99h@gmail.com>
2023-05-25 11:20:00 +0300

    p16p18

---
+++ P16/P16/P16.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\a";
+
+    cout << "Dear Sir\\Madam,\n\n";
+    cout << "How are you?\n\n";
+    cout << "My name is \"Abdallah\", nice to meet you.\n\n" << endl;
+
+    cout << "Ali\tAhmed\tLina\n";
+    cout << "Fadi\tZain\tMona" << endl;
+
+    return 0;
+}
---
+++ P18-1/P18-1/P18-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3;
+    cout << "Enter Number 1:" << endl;
+    cin >> num1;
+    cout << "Enter Number 2:" << endl;
+    cin >> num2;
+    cout << "Enter Number 3:" << endl;
+    cin >> num3;
+    cout << endl;
+
+    cout << num1 << " +\n";
+    cout << num2 << " +\n";
+    cout << num3 << endl;
+    cout << "_____________________________________\n\n";
+    cout << "Total = " << num1 + num2 + num3 << endl;
+
+
+    return 0;
+}
