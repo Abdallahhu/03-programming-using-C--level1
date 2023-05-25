286bfefa8161aa9b46ea3bf8721f8d86befc1248
Abdallahhn <abdallah.a99h@gmail.com>
2023-05-25 11:14:12 +0300

    p12p16

---
+++ p12/p12/p12.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "********************************\n";
+    cout << "Name: " << "Abdallah Hussein.\n";
+    cout << "Age: " << "24." << std::endl;
+    cout << "City: " << "Damascus.\n";
+    cout << "Country: " << "Syria.\n";
+    cout << "********************************\n\n\n";
+
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n\n\n";
+
+    cout << "I Love Programming!\n\n";
+    cout << "I promiseto be best developer ever!\n\n";
+    cout << "I know it will take some time to practice, but I will achieve my gool.\n\n";
+    cout << "Best Regards.\n";
+    cout << "Abdallah Hussein.\n\n\n";
+
+    cout << "*     *\n";
+    cout << "*     *\n";
+    cout << "*******\n";
+    cout << "*     *\n";
+    cout << "*     *\n";
+
+    return 0;
+}
